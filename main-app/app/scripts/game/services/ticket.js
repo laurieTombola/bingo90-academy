c3d905c53bcd64d7ab92c04879faa8c60f6f9d9e
Laurie Athey <laurie.athey@tombola.co.uk>
2015-11-17 17:19:56 +0000

    Layout update and the me.orderNumbers function has been updated to be more readable

--- main-app/app/scripts/game/services/ticket.js
+++ main-app/app/scripts/game/services/ticket.js
@@ -2,46 +2,47 @@
     'use strict';
 
     angular.module('Tombola.Games.Bingo90.Values').
-        service('Ticket', function(){
+        service('Ticket', function () {
             var me = this;
             me.unorderedNumbers = [];
             me.orderedNumbers = [];
             me.marks = [];
 
-            me.createTicket = function(numberString){
-                if(numberString.length !== 30) return;
+            me.createTicket = function (numberString) {
+                if (numberString.length !== 30) {
+                    return;
+                }
                 var j;
-                for(j = 0; i< 27; i++){
+                for (j = 0; i < 27; i++) {
                     marks[j] = "";
                 }
                 me.unorderedNumbers = [];
-                for(var i = 0; i < numberString.length; i += 2){
-                    var number = numberString[i].toString()+numberString[i+1].toString();
+                for (var i = 0; i < numberString.length; i += 2) {
+                    var number = numberString[i].toString() + numberString[i + 1].toString();
                     me.unorderedNumbers.push(number.toString());
                 }
                 me.orderNumbers();
             };
 
-            me.orderNumbers = function(){
-                var i;
-                for(i = 0; i<me.unorderedNumbers.length; i++){
-                    me.orderedNumbers[parseInt((9*(Math.floor(i/5))))+parseInt(me.eightOrLess(me.unorderedNumbers[i][0]))] = me.unorderedNumbers[i];
+            me.orderNumbers = function () {
+                var unorderedNumbersIndex;
+                for (unorderedNumbersIndex = 0; unorderedNumbersIndex < me.unorderedNumbers.length; unorderedNumbersIndex++) {
+                    var currentNumber = parseInt(me.unorderedNumbers[unorderedNumbersIndex]),
+                        row = Math.floor(unorderedNumbersIndex / 5),
+                        rowStartingCell = parseInt(9 * row),
+                        column = me.unorderedNumbers[unorderedNumbersIndex] !== "90" ? Math.floor(currentNumber / 10) : 8;
+                    me.orderedNumbers[rowStartingCell + column] = me.unorderedNumbers[unorderedNumbersIndex];
                 }
             };
 
-            me.eightOrLess = function(number){
-                return number < 9 ? number : 8;
-            };
-
-            me.checkMark = function(lastCall){
-                if(me.orderedNumbers.indexOf(lastCall.toString()) !== -1){
+            me.checkMark = function (lastCall) {
+                if (me.orderedNumbers.indexOf(lastCall.toString()) !== -1) {
                     me.marks[me.orderedNumbers.indexOf(lastCall.toString())] = 'marked';
-
                 }
             };
 
-            me.calcToGo = function(stage){
-                switch(stage){
+            me.calcToGo = function (stage) {
+                switch (stage) {
                     case "One Line":
                         //return me.calcOneLineToGo();
                         return me.calcFullHouseToGo();
@@ -52,45 +53,49 @@
                 }
             };
 
-            me.calcOneLineToGo = function(){
-                var row, column, toGos = [5,5,5], bestToGo = 5;
-                for(row = 0; row < 3; row++){
-                    for(column = 0; column < 9; column++){
-                        if(me.marks[column+(row*9)] === "marked"){
+            me.calcOneLineToGo = function () {
+                var row, column, toGos = [5, 5, 5], bestToGo = 5;
+                for (row = 0; row < 3; row++) {
+                    for (column = 0; column < 9; column++) {
+                        if (me.marks[column + (row * 9)] === "marked") {
                             toGos[row] -= 1;
                         }
                     }
                 }
                 console.log(toGos);
-                if(toGos[0] <= toGos[1] && toGos[0] <= toGos[2]){
-                    console.log("Line One: "+ toGos[0]);
+                if (toGos[0] <= toGos[1] && toGos[0] <= toGos[2]) {
+                    console.log("Line One: " + toGos[0]);
                     return toGos[0];
                 }
-                else if(toGos[1] <= toGos[0] && toGos[1] <= toGos[2]){
-                    console.log("Line Two: "+ toGos[1]);
-                    return toGos[1];
-                }
-                else if(toGos[2] <= toGos[0] && toGos[2] <= toGos[1]){
-                    console.log("Line Three: "+ toGos[2]);
-                    return toGos[2];
+                else {
+                    if (toGos[1] <= toGos[0] && toGos[1] <= toGos[2]) {
+                        console.log("Line Two: " + toGos[1]);
+                        return toGos[1];
+                    }
+                    else {
+                        if (toGos[2] <= toGos[0] && toGos[2] <= toGos[1]) {
+                            console.log("Line Three: " + toGos[2]);
+                            return toGos[2];
+                        }
+                    }
                 }
             };
 
-            me.calcTwoLinesToGo = function(){
+            me.calcTwoLinesToGo = function () {
 
             };
 
-            me.calcFullHouseToGo = function(){
+            me.calcFullHouseToGo = function () {
                 var i, marked = 15;
-                for(i = 0; i < 27; i++){
-                    if(me.marks[i] === "marked"){
+                for (i = 0; i < 27; i++) {
+                    if (me.marks[i] === "marked") {
                         marked -= 1;
                     }
                 }
                 return marked;
             };
 
-            me.reset = function(){
+            me.reset = function () {
                 me.unorderedNumbers = [];
                 me.orderedNumbers = [];
                 me.marks = [];
