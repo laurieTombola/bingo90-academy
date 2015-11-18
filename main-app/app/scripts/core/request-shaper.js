acc7bd75110c763d2b3e07bb89876889c5377f2b
Laurie Athey <laurie.athey@tombola.co.uk>
2015-11-18 11:33:33 +0000

    Request Shaper laid out and injected UserDetails to avoid needing it elsewhere.

--- main-app/app/scripts/core/request-shaper.js
+++ main-app/app/scripts/core/request-shaper.js
@@ -2,70 +2,70 @@
     'use strict';
 
     angular.module('Tombola.Games.Bingo90.API').
-        service('RequestShaper', [function(){
+        service('RequestShaper', ['UserDetails', function (userDetails) {
             var me = this;
 
-            me.makeLoginRequest = function(username, password){
+            me.makeLoginRequest = function (username, password) {
                 return {
                     method: 'POST',
                     url: 'http://eutaveg-01.tombola.emea:30069/users/login',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    data: {"username":username, "password":password}
+                    data: {"username": username, "password": password}
                 };
             };
 
-            me.makeLogoutRequest = function(token){
+            me.makeLogoutRequest = function () {
                 return {
                     method: 'POST',
                     url: 'http://eutaveg-01.tombola.emea:30069/users/logout',
                     headers: {
-                        'x-token': token,
+                        'x-token': userDetails.data.token,
                         'Content-Type': 'application/json'
                     }
                 };
             };
 
-            me.makePurchaseRequest = function(token){
+            me.makePurchaseRequest = function () {
                 return {
                     method: 'POST',
                     url: 'http://eutaveg-01.tombola.emea:30069/game/buyticket',
                     headers: {
-                        'x-token':token,
+                        'x-token': userDetails.data.token,
                         'Content-Type': 'application/json'
                     },
                     data: {
                         gameId: 1,
-                        userId:"drwho",
-                        balance: 100000
+                        userId: userDetails.data.userId,
+                        balance: userDetails.data.balance
                     }
                 };
             };
 
-            me.makeCallRequest = function(token, callNumber){
+            me.makeCallRequest = function (callNumber) {
                 return {
                     method: 'POST',
                     url: 'http://eutaveg-01.tombola.emea:30069/game/getcall',
                     headers: {
-                        'x-token':token,
+                        'x-token': userDetails.data.token,
                         'Content-Type': 'application/json'
                     },
                     data: {
                         gameId: 1,
-                        userId:"drwho",
-                        balance: 100000,
-                        callnumber:callNumber
+                        userId: userDetails.data.userId,
+                        balance: userDetails.data.balance,
+                        callnumber: callNumber
                     }
                 };
             };
 
-            me.makeNextGameRequest = function(token){
+            me.makeNextGameRequest = function () {
                 return {
                     method: 'GET',
                     url: 'http://eutaveg-01.tombola.emea:30069/game/next',
                     headers: {
-                        'x-token': token,
+                        'x-token': userDetails.data.token,
                         'Content-Type': 'application/json'
                     }
                 };
